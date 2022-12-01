b61263eafedbb466bd652742ea91b226663d3fda
adamgaafar <dodogaafar.ag@gmail.com>
2022-12-01 14:26:45 +0200

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,10 +8,30 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  /*  val testInput = readInput("Day01_test")
+    check(part1(testInput) == 1)*/
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    println(input)
+
+    var currentList = mutableListOf<Int>()
+    var maxValue = mutableListOf<Int>()
+    var maxTopThree = mutableListOf<Int>()
+
+    for (i in input){
+        if (i == ""){
+            maxValue.add(currentList.sum())
+            currentList.clear()
+        }else{
+            currentList.add(i.toInt())
+        }
+    }
+    for (i in 0..2){
+        var curr = maxValue.maxOf { it }
+        maxTopThree.add(curr)
+        maxValue.remove(curr)
+    }
+    println(maxValue.maxOf { it })
+    println(maxTopThree.sum())
 }
