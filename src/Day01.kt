03a4940e0a225c99c4f670279196f4d372d9e663
adamgaafar <dodogaafar.ag@gmail.com>
2022-12-02 13:20:31 +0200

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,37 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-  /*  val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)*/
-
+    //global variables
     val input = readInput("Day01")
-
-    println(input)
-
     var currentList = mutableListOf<Int>()
     var maxValue = mutableListOf<Int>()
     var maxTopThree = mutableListOf<Int>()
 
-    for (i in input){
-        if (i == ""){
-            maxValue.add(currentList.sum())
-            currentList.clear()
-        }else{
-            currentList.add(i.toInt())
+    fun part1(input: List<String>): Int {
+        for (i in input){
+            if (i == ""){
+                maxValue.add(currentList.sum())
+                currentList.clear()
+            }else{
+                currentList.add(i.toInt())
+            }
         }
+        return  maxValue.maxOf { it }
     }
-    for (i in 0..2){
-        var curr = maxValue.maxOf { it }
-        maxTopThree.add(curr)
-        maxValue.remove(curr)
+
+    fun part2(input: List<String>): Int {
+        for (i in 0..2){
+            var curr = maxValue.maxOf { it }
+            maxTopThree.add(curr)
+            maxValue.remove(curr)
+        }
+
+        return  maxTopThree.sum()
     }
-    println(maxValue.maxOf { it })
-    println(maxTopThree.sum())
+
+
+    // test if implementation meets criteria from the description, like:
+  /*  val testInput = readInput("Day01_test")
+    check(part1(testInput) == 1)*/
+    println(part1(input))
+    println(part2(input))
+
+
+
 }
